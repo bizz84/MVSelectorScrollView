e0682c1741c8c84486aeb418aad6a8433069754b
Andrea Bizzotto <bizz84@gmail.com>
2013-04-12 16:55:18 +0100

    Initial Commit

---
+++ HorizontalScrollViewSelector/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HorizontalScrollViewSelector
+//
+//  Created by Andrea Bizzotto on 12/04/2013.
+//  Copyright (c) 2013 musevisions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HorizontalScrollViewSelector/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HorizontalScrollViewSelector
+//
+//  Created by Andrea Bizzotto on 12/04/2013.
+//  Copyright (c) 2013 musevisions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
